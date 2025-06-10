528836a21cfdf1ea87198e2ecd0c6f807a328d0c
SSO <sso@d.co>
2025-06-10 11:19:43 +0200

    added HEader and loaded meals from backend

---
+++ src/components/Meals.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export default function Meals() {
+	const [loadedMeals, setLoadedMeals] = useState([]);
+
+	useEffect(() => {
+		async function fetchMeals() {
+			const response = await fetch('http://localhost:3000/meals');
+
+			if (!response.ok) {
+				console.log(response);
+			}
+
+			const meals = await response.json();
+			setLoadedMeals(meals);
+		}
+		fetchMeals();
+	}, []);
+
+	return (
+		<ul id='meals'>
+			{loadedMeals.map((meal) => (
+				<li key={meal.id}>{meal.name}</li>
+			))}
+		</ul>
+	);
+}
